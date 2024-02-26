8756437fcfedc4dabb0e9889a2fb6a8f272ad335
DiegoAlb09 <diegoalb04hat@gmail.com>
2024-02-26 13:27:20 -0600

    Practicas de Machine Learning

---
+++ Aprendizaje_Inteligente/AI_Perceptron_FlorIris_Graficas_Practica4.R
@@ -0,0 +1,15 @@
+# Universidad Autonoma de Aguascalientes
+# Departamento: Ciencias de la Computación
+# Carrera: Ingenieria en Computación Inteligente
+# Curso: Aprendizaje Inteligente (Machine Learning)
+# Maestro: Dr. Francisco Javier Luna Rosas
+# Alumno: Diego Alberto Aranda Gonzalez
+# Semestre: Enero - Junio 2024
+# Practica 4: Algoritmo del perceptron simple aplicado a la clasificación de la flor Iris con graficas
+
+# Importar librerias
+#library(ggplot2)
+#library(reshape2)
+
+# Cargar datos
+read.csv("iris.csv")
