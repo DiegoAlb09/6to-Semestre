3203997441b192c54697e130c279db070c99b811
DiegoAlb09 <diegoalb04hat@gmail.com>
2024-04-29 10:49:23 -0600

    Ultimo avance

--- Introducción_Sistemas_Operativos/Procesos_Algoritmos.py
+++ Introducción_Sistemas_Operativos/Procesos_Algoritmos.py
@@ -15,6 +15,47 @@ def menu():
   print("3.- Salir")
   opcion = int(input("Selecciona una opción: "))
   return opcion
+def buscarTiempoEspera(procesos,n,bt,wt,quantum):
+  rem_bt = [0] * n 
+  #Copiamos el tiempo de ejecución de los procesos en rem_bt
+  for i in range(n):
+    rem_bt[i] = bt[i]
+  t = 0
+
+  while(1):
+    done = True
+    for i in range(n):
+      if (rem_bt[i] > 0):
+        done = False
+        if (rem_bt[i] > quantum):
+          t += quantum
+          rem_bt[i] -= quantum
+        else:
+          t = t + rem_bt[i]
+          wt[i] = t - bt[i]
+          rem_bt[i] = 0
+    if (done == True):
+      break
+
+def findTurnAroundTime(procesos,n,bt,wt,tat):
+  for i in range(n):
+    tat[i] = bt[i] + wt[i]
+
+def findavgTime(procesos,n,bt,quantum):
+  wt = [0] * n
+  tat = [0] * n
+  buscarTiempoEspera(procesos,n,bt,wt,quantum)
+  findTurnAroundTime(procesos,n,bt,wt,tat)
+  total_wt = 0
+  total_tat = 0
+  print("Proceso\tTiempo de llegada\tTiempo de Ejecución\tTiempo de Espera\tTiempo de Retorno")
+  for i in range(n):
+    total_wt += wt[i]
+    total_tat += tat[i]
+    print(str(i + 1) + "\t" + str(int(procesos[i])) + "\t\t\t" + str(int(bt[i])) + "\t\t\t" + str(wt[i]) + "\t\t\t" + str(tat[i]))
+  print("Tiempo de espera promedio: ", total_wt / n)
+  print("Tiempo de retorno promedio: ", total_tat / n)
+  
 
 #Función principal
 def main():
@@ -55,41 +96,14 @@ def main():
     elif opcion == 2:
       print("Algoritmo Round Robin")
       numProcesos = int(input("Ingresa el número de procesos: "))
+      procesos = np.zeros(numProcesos)
+      for i in range(numProcesos):
+        procesos[i] = int(input("Ingresa el tiempo de llegada del proceso " + str(i + 1) + ": "))
+      tiempoProceso = np.zeros(numProcesos)
+      for i in range(numProcesos):
+        tiempoProceso[i] = int(input("Ingresa el tiempo del proceso " + str(i + 1) + ": "))
       quantum = int(input("Ingresa el quantum: "))
-      #Se crea una matriz para almacenar el orden de llegada y el tiempo de cada proceso
-      procesos = np.zeros((numProcesos, 2))
-      for i in range(numProcesos):
-        procesos[i][0] = int(input("Ingresa el orden de llegada del proceso " + str(i + 1) + ": "))
-        procesos[i][1] = int(input("Ingresa el tiempo del proceso " + str(i + 1) + ": "))
-      
-      #Se ordenan los procesos por orden de llegada
-      procesos = procesos[procesos[:,0].argsort()]
-
-      #Se calcula el tiempo de espera y el tiempo de retorno
-      tiempoInicio = 0
-      tiempoRetorno = 0
-      tiempoEsperaTotal = 0
-      tiempoRetornoTotal = 0
-      tiempoTotal = 0
-      tiempoE = 0
-      tiempoP = 0 
-      auxProcesos = np.zeros((numProcesos, 2))
-      print("Proceso\tTiempo de llegada\tTiempo de Ejecución\tTiempo de Inicio\tTiempo Finalizacion\tTiempo Total\tTiempo de Espera\tP")
-
-      for i in range(numProcesos):
-          #Calculo del tiempo de inicio con el quantum
-          tiempoInicio = tiempoRetorno
-          auxProcesos[i][1] = procesos[i][1]
-          #Si el tiempo de ejecución es mayor al quantum
-          if procesos[i][1] > quantum:
-            auxProcesos[i][1] -= quantum
-            tiempoRetorno += quantum
-          else:
-            tiempoRetorno += auxProcesos[i][1]
-            auxProcesos[i][1] = 0
-          print(str(i + 1) + "\t" + str(int(procesos[i][0])) + "\t\t\t" + str(int(procesos[i][1])) + "\t\t\t" + str(tiempoInicio) + "\t\t\t" + str(tiempoRetorno) + "\t\t\t" + str(tiempoTotal) + "\t\t\t" + str(tiempoE) + "\t\t\t" + str(tiempoP))
-      #print("Tiempo Total promedio: ", tiempoEsperaTotal / numProcesos)
-      #print("Tiempo de retorno promedio: ", tiempoRetornoTotal / numProcesos)
+      findavgTime(procesos,numProcesos,tiempoProceso,quantum)
     opcion = menu()
 
 main()
